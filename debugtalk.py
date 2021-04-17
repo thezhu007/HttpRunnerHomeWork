8d20de7fedcd482eef35075ba69a7f655bdd8eba
thezhu007 <featherwit96@163.com>
2021-04-17 10:48:42 +0800

    HttpRunner作业提交

---
+++ debugtalk.py
@@ -0,0 +1,8 @@
+def setup_hook_testcase(casename):
+    print("----------%s测试用例执行开始----------"%casename)
+
+def teardown_hook_testcase(casename):
+    print("----------%s测试用例执行结束----------"%casename)
+
+def creat_tags():
+    return ["new_04","new_05","new_06"]
